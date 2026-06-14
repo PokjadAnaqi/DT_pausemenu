b23a53c1cebcc7ae9a1e1b2503167528976aeda6
PokjadAnaqi <alypheyjad1@gmail.com>
2026-06-14 15:33:35 +0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version "cerulean"
+use_fxv2_oal "yes"
+lua54 "yes"
+game "gta5"
+version "1.0.0"
+description "A simple pausemenu system React + Mantine"
+name 'DT_pausemenu'
+author "PokjadAnaqi"
+
+shared_scripts {
+  '@ox_lib/init.lua',
+  '@qbx_core/modules/lib.lua',
+  "shared/**/*.lua"
+}
+
+client_scripts {
+  '@qbx_core/modules/playerdata.lua',
+  "client/**/*.lua"
+}
+
+server_scripts {
+  "server/**/*.lua"
+}
+
+ui_page "web/build/index.html"
+
+files {
+  "web/build/index.html",
+  "web/build/**/*",
+  'web/images/*.png'
+}
