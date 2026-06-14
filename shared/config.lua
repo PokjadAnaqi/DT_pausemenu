d24864a9deabc5f9c745fe2f906d1f4782597a61
PokjadAnaqi <alypheyjad1@gmail.com>
2026-06-14 15:29:19 +0800

    Add files via upload

--- shared/config.lua
+++ shared/config.lua
@@ -1,3 +1,30 @@
 Config = {}
+Config.Logo = "https://r2.fivemanage.com/ofEPga1iSUQyhdyfxzTMA/DeltaHorizonMain500.png"
+Config.ImageMap = "https://r2.fivemanage.com/ofEPga1iSUQyhdyfxzTMA/fantasy.png"
+Config.CurrencySymbol = "DTC"
+Config.DirtySymbol = "DBC"
+Config.DiscordLink = "https://discord.gg/e4mvRC5q7u"
 
-Config.ImageMap = "nui://krs_pausemenu/web/images/map.png"
+Config.Actions = {
+    {
+        title = "Quest Board",
+        subtitle = "Open Quest Board",
+        icon = "quest",
+        type = "client",
+        event = "quest:open"
+    },
+    {
+        title = "VIP",
+        subtitle = "VIP Menu",
+        icon = "crown",
+        type = "server",
+        event = "vip:open"
+    },
+    {
+        title = "CLASS",
+        subtitle = "Open Class Menu",
+        icon = "package",
+        type = "command",
+        event = "classmenu"
+    }
+}
